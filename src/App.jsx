efcf3c548fc62283061768c1ed475cb89536929a
shykulamykhailo <lucamike332@gmail.com>
2024-09-07 23:23:27 +0300

    create and connect database using supabase

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Users from './pages/Users';
 import Settings from './pages/Settings';
 import Bookings from './pages/Bookings';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import AppLayout from './ui/AppLayout';
 
 function App() {
     return (
@@ -14,16 +16,20 @@ function App() {
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
-                    <Route
-                        index
-                        element={<Navigate replace to="dashboard" />}
-                    />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="cabins" element={<Cabins />} />
-                    <Route path="users" element={<Users />} />
-                    <Route path="settings" element={<Settings />} />
-                    <Route path="account" element={<Account />} />
-                    <Route path="bookings" element={<Bookings />} />
+                    <Route element={<AppLayout />}>
+                        <Route
+                            index
+                            element={<Navigate replace to="dashboard" />}
+                        />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                        <Route path="bookings" element={<Bookings />} />
+                    </Route>
+
+                    <Route path="login" element={<Login />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
