ea86a338e1eba63c34af173d017e5dd2c6415111
shykulamykhailo <lucamike332@gmail.com>
2024-09-18 21:19:14 +0300

    bug fixes

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import Logo from './Logo';
 import MainNav from './MainNav';
-import { useCabins } from '../features/cabins/useCabins';
-import Uploader from '../data/Uploader';
 
 const StyledSidebar = styled.aside`
     background-color: var(--color-grey-0);
@@ -20,7 +18,6 @@ function Sidebar() {
         <StyledSidebar>
             <Logo />
             <MainNav />
-            <Uploader />
         </StyledSidebar>
     );
 }
