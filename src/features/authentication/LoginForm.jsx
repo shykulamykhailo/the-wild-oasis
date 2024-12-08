423c4ad458b5c9568be9792f86e1fc8d58796419
shykulamykhailo <lucamike332@gmail.com>
2024-12-08 15:19:40 +0200

    feat: add default login and password

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('test@test.com');
+    const [password, setPassword] = useState('testtest');
 
     const { login, isLoading } = useLogin();
 
